daf6f67330ac75e26b82dcb473dd8973ef9cf1be
Diane27 <dianetobit@gmail.com>
2017-09-16 13:37:39 -0700

    populate the data section with comments

--- src/components/main.js
+++ src/components/main.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class Main extends Component {
   render() {
     return (
-      <div>
-        <h2>hello world</h2>
-      </div>
+      <div className="jumbotron jumbotron-fluid">
+		  <div className="container">
+		    <h1 className="display-3">Welcome to Our Page</h1>
+		    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+		  </div>
+		</div>
     );
   }
 }
--- src/data/data_comments.js
+++ src/data/data_comments.js
@@ -11,6 +11,54 @@ const DATACOMMENTS = [
         child_id: 2,
         comments: '222222 test 2222'
     },
+    {
+        classroom_id: 1,
+        parent_id: 3,
+        child_id: 3,
+        comments: 'I would like to volunteer to drive the kids to the brooklyn Museum.'
+    },
+    {
+        classroom_id: 1,
+        parent_id: 4,
+        child_id: 4,
+        comments: 'How can I change my choice? We are going on a vacation next week we will not be able to make it for the trip'
+    },
+    {
+        classroom_id: 1,
+        parent_id: 5,
+        child_id: 5,
+        comments: "Thanks to Ms. Thompson for volunteering. We are glad you decided to dedicate time to our children"
+    },
+    {
+        classroom_id: 1,
+        parent_id: 6,
+        child_id: 6,
+        comments: 'I will be a volunteer too. who else is volunteering?'
+    },
+    {
+        classroom_id: 1,
+        parent_id: 7,
+        child_id: 7,
+        comments: 'Are you still in need for volunteer?'
+    },
+    {
+        classroom_id: 1,
+        parent_id: 8,
+        child_id: 8,
+        comments: "This is definitely worth it. My kid had so much fun last time he couldn't stop talking about it."
+    },
+    {
+        classroom_id: 1,
+        parent_id: 9,
+        child_id: 9,
+        comments: 'This is a great initiative.'
+    },
+    {
+        classroom_id: 1,
+        parent_id: 10,
+        child_id: 10,
+        comments: "I love going to the zoo with my kids I can't wait. I will be volunteer."
+    },
 ]
 
 export default DATACOMMENTS;
