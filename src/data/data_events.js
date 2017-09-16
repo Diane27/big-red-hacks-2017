a01213b8ad0660fb8f9b51f5ae3e614f0f67d02a
edhou <edward.h@live.ca>
2017-09-16 16:57:34 -0400

    Added content for events

--- src/data/data_events.js
+++ src/data/data_events.js
@@ -6,24 +6,76 @@ const DATAEVENTS = [
             [1505548100,1505548128],
             [1505548157,1505548151]
         ],
-        "topics" : ["technology"],
+        "topics" : [["Art"],["Technology"]],
         "leason_plans" : [1,2,3],
         "details": "Come celebrate Maker Faire's 8th anniversary in New York, showcasing creative and resourceful people in the areas of science and technology, engineering, food, and arts and crafts.",
         "latlng" : [40.746834, -73.852159],
         "image": "https://img.grouponcdn.com/deal/3590a7a647a047bfa1a1affd4e248ebb/bb/v1/c700x420.jpg"
     },
-    {   
+    {
         "id" : 2,
         "name" : "American Natural Museum of History",
         "dates" : [
             [1505548100,1505548128],
             [1505548157,1505548151]
         ],
-        "topics" : ["history"],
+        "topics" : [["History"],["Science"]],
         "leason_plans" : [4,6],
         "details": "The American Museum of Natural History, located on the Upper West Side of Manhattan, New York City, is one of the largest museums in the world.",
         "latlng" : [40.781384, -73.974371],
         "image" : "http://media.ruebarue.com/photos/places/4785227343855616/1-american-museum-of-natural-history-dinasour.jpg"
+    },
+    {
+        "id" : 3,
+        "name" : "New York Transit Museum",
+        "dates" : [
+          [1505548100,1505548128],
+          [1505548157,1505548151]
+        ],
+        "topics" : [["History"],["Technology"]],
+        "lesson_plans" : [7,8],
+        "details": "School programs at the Transit Museum explore how transportation shaped the development of New York City over the course of 200 years. Programs use a mix of inquiry, observation, hands-on activities and Universal Design to engage learners of all ages and abilities. ",
+        "latlng" : [40.781384, -73.974371],
+        "image" : "http://www.nytransitmuseum.org/wp-content/uploads/2016/09/Time-Warner-Cable-2015-Credit-Shahar-Azran-2-1024x683.jpg"
+    },
+    {
+        "id" : 4,
+        "name" : "Museum of Food and Drink",
+        "dates" : [
+          [1505548100,1505548128],
+          [1505548157,1505548151]
+        ],
+        "topics" : [["Society"], ["History"], ["Technology"]],
+        "lesson_plans" : [8,9],
+        "details": "MOFAD seeks to advance the public understanding of the culture, history, science, production, and commerce of food and drink. The museum offers hands-on guided tours based on age-appropriate lesson plans. We encourage students to think critically and creatively about food, as well as the stories and history connected to it.",
+        "latlng" : [40.781384, -73.974371],
+        "image" : "https://static1.squarespace.com/static/56edca6e20c64724c3749085/t/583affe76b8f5bb63a4a86b8/1480261617771/?format=750w"
+    },
+    {
+        "id" : 5,
+        "name" : "Brookyln Navy Yard Center",
+        "dates" : [
+          [1505548100,1505548128],
+          [1505548157,1505548151]
+        ],
+        "topics" : [["History"], ["Technology"]],
+        "lesson_plans" : [9,10],
+        "details": "Students visiting the Brooklyn Navy Yard will learn to think like historians and engineers through hands-on investigation of Brooklyn’s material culture, art, and written documents—all of which form intriguing windows into the history of America, and the development of New York City.",
+        "latlng" : [40.781384, -73.974371],
+        "image" : "https://blankslatepages.s3.amazonaws.com/56f460429b129-galleries-500x333.png"
+    },
+    {
+        "id" : 6,
+        "name" : "New York Aquarium",
+        "dates" : [
+          [1505548100,1505548128],
+          [1505548157,1505548151]
+        ],
+        "topics" : [["Biology"], ["Society"]],
+        "lesson_plans" : [11,12],
+        "details": "The New York Aquarium is the oldest continually operating aquarium in the United States. The Aquarium's mission is to save wildlife and wild places worldwide through science, conservation action, education, and inspiring people to value nature.The facility occupies 14 acres and boasts 266 species of aquatic wildlife",
+        "latlng" : [40.781384, -73.974371],
+        "image" : "https://media.timeout.com/images/102718559/image.jpg"
     }
 ]
 
