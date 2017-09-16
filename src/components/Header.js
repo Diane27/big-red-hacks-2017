dc6430d1050405f7f7da46a06f19e9ba6201df76
Diane27 <dianetobit@gmail.com>
2017-09-16 05:59:27 -0700

    create form and header for landing page

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+    	<div>
+    		<section className="header">
+	    		<h1><big>OUR APP</big></h1>
+			</section>
+	      
+		</div>
+    );
+  }
+}
+
